5a392632935c3c52783c9f1c5c4fb8b4913c8a2f
utkarshagrawal <utkarshagrawal09jan@gmail.com>
2024-04-12 21:05:08 +0530

    Updated footer

--- memes/components/footer.jsx
+++ memes/components/footer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./footer.css"
 
-export function Foot(){
+export function Foot() {
 
   return (
     <div className="footer">
-      <h4>Copyright © 2023 Created by notacloud</h4>
+      <h4>Copyright © 2023 Created by utkarshagrrawal</h4>
     </div>
   )
 
